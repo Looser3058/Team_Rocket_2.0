a900dd7faa8887f4c41667141919cf329f1d907e
NicolasAudicio <nicolasaudicio95@gmail.com>
2023-09-24 18:06:15 -0300

    ajustando atributos

--- Python/pedidos.py
+++ Python/pedidos.py
@@ -1,16 +1,11 @@
 # pedidos.py
 
 class Pedidos:
-    """Class representing a pedidos."""
-    def __init__(self, producto, cantidad):
-        self.producto = producto
-        self.cantidad = cantidad
-
-    def calcular_total(self):
-        """Returns the total cost of the product."""
-        return self.producto.precio * self.cantidad
+    
+    def __init__(self, nropedido):
+        self.nropedido = nropedido
 
     def __str__(self):
-        return f"Producto: {self.producto.nombre}, Cantidad: {self.cantidad}, Total: {self.calcular_total()}"
+        return f"Producto: {self.nropedido}"
     
     
--- Python/productos.py
+++ Python/productos.py
@@ -3,15 +3,15 @@
 class Producto:
     categoria=["Papelería","Accesorios"]
 
-    def __init__(self, nombre, precio, categoria, stock):
+    def __init__(self, nombre, precio, categoria, stock, id):
         self.nombre = nombre
         self.precio = precio
         self.categoria = categoria
         self.stock = stock
+        self.id=id
 
-    def set_categoria(self):
-        return self.categoria
+    
 
     def __str__(self):
-        return f"Producto: {self.nombre}, Precio: {self.precio}, Categoria: {self.categoria}, Stock: {self.stock}"
+        return f"Producto: {self.nombre}, Precio: {self.precio}, Categoria: {self.categoria}, Stock: {self.stock}, id:{self.id}"
 
